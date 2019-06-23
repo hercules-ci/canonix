77096d33b4c3e9e72a3371d890550632f7988cd8
Robert Hensing <robert@roberthensing.nl>
2019-06-23 15:18:40 +0200

    Add -lib and -strict builds for CI purposes

--- default.nix
+++ default.nix
@@ -3,4 +3,8 @@
   canonix =
     pkgs.haskell.lib.justStaticExecutables pkgs.canonixHaskellPackages.canonix;
   
+  canonix-lib = pkgs.canonixHaskellPackages.canonix;
+  canonix-strict =
+    pkgs.haskell.lib.buildStrictly pkgs.canonixHaskellPackages.canonix;
+  
 }
