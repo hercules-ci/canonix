e316fca4bb33993e5ade0d1f42fe6628b696cc9d
Robert Hensing <robert@roberthensing.nl>
2019-07-02 13:43:15 +0200

    Format

--- canonix/src/Canonix/Monad.hs
+++ canonix/src/Canonix/Monad.hs
@@ -20,7 +20,7 @@ module Canonix.Monad
   , askParent
   , asksParent
   , tellChildren
-  
+
     -- * Bottom-up data flow
     --
     -- Info about subtrees ("synthesized")
@@ -31,15 +31,16 @@ module Canonix.Monad
   , write
   , censorWrites
   , throw
-
-  ) where
+  )
+where
 
 import           Control.Monad.Free
 
 newtype Fmt inh syn o e a = Fmt { fromFmt :: Free (FormatterEffect inh syn () o e) a }
   deriving (Functor, Applicative, Monad)
 
-runFmt :: Monoid syn => Fmt inh syn o e a -> inh -> Progress o e (Result syn () a)
+runFmt
+  :: Monoid syn => Fmt inh syn o e a -> inh -> Progress o e (Result syn () a)
 runFmt (Fmt m) inh = runFormatter m inh ()
 
 askParent :: Fmt inh syn o e inh
@@ -51,8 +52,13 @@ asksParent f = Fmt $ Free $ AskParent $ pure . f
 tellChildren :: inh -> Fmt inh syn o e a -> Fmt inh syn o e a
 tellChildren inh (Fmt m) = Fmt $ Free $ TellChildren m inh pure
 
-censorChildren :: Monoid syn' => Fmt inh syn' o e a -> (syn' -> a -> Fmt inh syn o e b) -> Fmt inh syn o e b
-censorChildren (Fmt m) f = Fmt $ Free $ CensorChildren m (\syn' a -> fromFmt $ f syn' a)
+censorChildren
+  :: Monoid syn'
+  => Fmt inh syn' o e a
+  -> (syn' -> a -> Fmt inh syn o e b)
+  -> Fmt inh syn o e b
+censorChildren (Fmt m) f =
+  Fmt $ Free $ CensorChildren m (\syn' a -> fromFmt $ f syn' a)
 
 tellParent :: syn -> Fmt inh syn o e ()
 tellParent syn = Fmt $ Free $ TellParent syn (pure ())
@@ -60,8 +66,10 @@ tellParent syn = Fmt $ Free $ TellParent syn (pure ())
 write :: o -> Fmt inh syn o e ()
 write o = Fmt $ Free $ Write o (pure ())
 
-censorWrites :: Fmt inh syn o' e a -> ([o'] -> a -> Fmt inh syn o e b) -> Fmt inh syn o e b
-censorWrites (Fmt m) f = Fmt $ Free $ CensorWrites m (\o's a -> fromFmt $ f o's a)
+censorWrites
+  :: Fmt inh syn o' e a -> ([o'] -> a -> Fmt inh syn o e b) -> Fmt inh syn o e b
+censorWrites (Fmt m) f =
+  Fmt $ Free $ CensorWrites m (\o's a -> fromFmt $ f o's a)
 
 throw :: e -> Fmt inh syn o e a
 throw e = Fmt $ Free $ Throw e
@@ -93,46 +101,54 @@ data Result syn sib a = Result { resultSyn :: syn, resultSib :: sib, resultValue
 
 instance Applicative (Progress o e) where
   pure = Done
-  Step o c <*> r = Step o (c <*> r)
-  Exceptional e <*> _ = Exceptional e
-  Done f <*> Step o c = Step o (Done f <*> c)
-  Done _f <*> Exceptional e = Exceptional e
-  Done f <*> Done a = Done (f a)
+  Step o c       <*> r             = Step o (c <*> r)
+  Exceptional e  <*> _             = Exceptional e
+  Done        f  <*> Step o c      = Step o (Done f <*> c)
+  Done        _f <*> Exceptional e = Exceptional e
+  Done        f  <*> Done        a = Done (f a)
 instance Monad (Progress o e) where
-  Step o c >>= f = Step o (c >>= f)
+  Step o c      >>= f = Step o (c >>= f)
   Exceptional e >>= _ = Exceptional e
-  Done m >>= f = f m
+  Done        m >>= f = f m
 
-runFormatter :: Monoid syn => Free (FormatterEffect inh syn sib o e) a -> inh -> sib -> Progress o e (Result syn sib a)
+runFormatter
+  :: Monoid syn
+  => Free (FormatterEffect inh syn sib o e) a
+  -> inh
+  -> sib
+  -> Progress o e (Result syn sib a)
 runFormatter (Pure a) _inh sib = Done (Result mempty sib a)
 
  -- top-down
 runFormatter (Free (AskParent f)) inh sib = runFormatter (f inh) inh sib
 runFormatter (Free (TellChildren sub inh' f)) inh sib = do
   t <- runFormatter sub inh' sib
-  runFormatter (Free (TellParent (resultSyn t) $ f (resultValue t))) inh (resultSib t)
+  runFormatter (Free (TellParent (resultSyn t) $ f (resultValue t)))
+               inh
+               (resultSib t)
 
  -- bottom-up
 runFormatter (Free (CensorChildren sub f)) inh sib = do
   t <- runFormatter sub inh sib
   runFormatter (f (resultSyn t) (resultValue t)) inh (resultSib t)
-runFormatter (Free (TellParent syn a)) inh sib = (\r -> r { resultSyn = syn <> resultSyn r }) <$> runFormatter a inh sib
+runFormatter (Free (TellParent syn a)) inh sib =
+  (\r -> r { resultSyn = syn <> resultSyn r }) <$> runFormatter a inh sib
 
  -- left-to-right (beginning of file to end of file, sort of like preorder but
  --                allowing to 'revisit' the parent before moving on)
  --               (chaining in attribute grammars)
-runFormatter (Free Modify {}) _ _ = error "not implemented"
+runFormatter (Free Modify{}) _ _ = error "not implemented"
 
  -- streaming output
 runFormatter (Free (Write o a)) inh sib =
-  let x = runFormatter a inh sib
-  in Step o x
+  let x = runFormatter a inh sib in Step o x
 runFormatter (Free (CensorWrites sub f)) inh sib =
-  let
-    subProgress = runFormatter sub inh sib
-    go acc (Step a p) = go (acc . (a:)) p
-    go _ac (Exceptional e) = Exceptional e
-    go acc (Done t) = runFormatter (Free (TellParent (resultSyn t) $ f (acc []) (resultValue t))) inh (resultSib t)
-  in go id subProgress
-runFormatter (Free (Throw e)) _inh _sib =
-  Exceptional e
+  let subProgress = runFormatter sub inh sib
+      go acc (Step a p     ) = go (acc . (a :)) p
+      go _ac (Exceptional e) = Exceptional e
+      go acc (Done        t) = runFormatter
+        (Free (TellParent (resultSyn t) $ f (acc []) (resultValue t)))
+        inh
+        (resultSib t)
+  in  go id subProgress
+runFormatter (Free (Throw e)) _inh _sib = Exceptional e
